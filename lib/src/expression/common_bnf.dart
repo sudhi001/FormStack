d0b1396edb853dbd717a7c535c52d30512d3bbc8
Sudhi S <sudhis@live.com>
2023-04-13 20:16:49 +0530

    bug fix

--- lib/src/expression/common_bnf.dart
+++ lib/src/expression/common_bnf.dart
@@ -8,8 +8,8 @@ class StringExpressionEvaluator {
   StringExpressionEvaluator._();
   static bool evaluateCondition(String condition, String input) {
     var parts = condition.split(' ');
-    dynamic right = parts[1];
     var operator = parts[0];
+    dynamic right = parts.length > 1 ? parts[1] : "";
 
     switch (operator) {
       case '=':
